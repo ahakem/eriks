a302c68c9e9288114de5ead9ff3ddb996b9345b2
Ahmed Hakim <me@hakim.ninja>
2021-10-09 17:08:56 +0200

    showing data from endpoint

--- src/views/Products.js
+++ src/views/Products.js
@@ -1,25 +1,46 @@
 import { useEffect, useState } from "react";
-import { data, notCompared } from "../data/products";
-import { Container} from "@mui/material";
+import { notCompared } from "../data/products";
+import { Container } from "@mui/material";
 
 import Table from "../componenets/Table";
 import structureData from "../utils/structureComparableData";
+const axios = require("axios").default;
+
 const Products = () => {
   const [state, setstate] = useState(null);
+  const [OrignalData, setOrignalData] = useState(null);
 
   useEffect(() => {
-    structureData(data, notCompared, state, setstate);
+    axios
+      .get("http://5c35e7f96fc11c0014d32fcd.mockapi.io/compare/products")
+      .then(function (response) {
+        // handle success
+        structureData(response.data, notCompared, state, setstate);
+        setOrignalData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
   }, []);
 
+  const toggleProducts = (index, productState) => {
+    structureData(OrignalData, notCompared, state, setstate, {
+      index,
+      productState,
+    });
+  };
 
-  const toggleProducts = (index, productState) =>{
-    structureData(data, notCompared,state, setstate,{index, productState})
-  }
-
-  if (!state) return <div>Loading...</div>;
-  return <Container>
-      <Table toggleProducts={toggleProducts} state={state} columns={data.products} />
-      </Container>;
+  if (!state || !OrignalData) return <div>Loading...</div>;
+  return (
+    <Container>
+      <Table
+        toggleProducts={toggleProducts}
+        state={state}
+        columns={OrignalData.products}
+      />
+    </Container>
+  );
 };
 
 export default Products;
